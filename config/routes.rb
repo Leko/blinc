87bf0640ce7ac5ff95eb7a2d71d81fd7408de2e5
Shingo Inoue <inoue.shingo@hamee.co.jp>
2015-06-13 23:26:01 +0900

    chore(controller): Generate Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+
+  # Sessions
+  get '/logout' => 'sessions#destroy', :as => :logout
+
+  # Omniauth
+  get '/auth/:provider/callback', :to => 'sessions#callback'
+  post '/auth/:provider/callback', :to => 'sessions#callback'
 end
