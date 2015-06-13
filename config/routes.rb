c5720cb2f6928b5c64b3ddfd85913e75ea5cb309
Shingo Inoue <inoue.shingo@hamee.co.jp>
2015-06-14 02:05:53 +0900

    fix(routing): Arrange unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   get 'welcome/index'
   get 'welcome/term'
   get 'welcome/privacy'
 
-  root 'home#index'
   get 'home/index'
 
-  # Sessions
-  get '/logout' => 'sessions#destroy', :as => :logout
-
-  # Omniauth
   get '/auth/:provider/callback', :to => 'sessions#callback'
-  post '/auth/:provider/callback', :to => 'sessions#callback'
+  get '/logout' => 'sessions#destroy', :as => :logout
 end
